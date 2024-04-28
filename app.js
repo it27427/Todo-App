b93da5e4a95fc57cbe913f89fb08623436f1d3f7
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-28 13:37:19 +0600

    app initialize done

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+  const notFound = <h1>Page Not Found</h1>;
+  res.status(404).send(notFound);
+  res.end();
+});
+
+app.use((err, req, res, next) => {
+  const serverError = <h1>Internal Server Error!</h1>;
+  res.status(500).send(serverError);
+  res.end();
+});
 
 module.exports = app;
