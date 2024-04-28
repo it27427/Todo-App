b415b7401778df8aec1293da12e436d1b474f59d
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-28 13:32:50 +0600

    routes, controller done

---
+++ config/config.js
@@ -0,0 +1,13 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const dev = {
+  app: {
+    port: process.env.PORT || 3001,
+  },
+  db: {
+    // url: process.env.DB_URL || 'mongodb://localhost:27017/todoDB'
+  },
+};
+
+module.exports = dev;
---
+++ controllers/home.controller.js
@@ -0,0 +1,9 @@
+const getHomeController = (req, res) => {
+  const test = <h1>Hello from Home route</h1>;
+  res.send(test);
+  res.end();
+};
+
+module.exports = {
+  getHomeController,
+};
