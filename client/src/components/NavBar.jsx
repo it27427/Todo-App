05c5e0ebcecfae5e0de3c07a874b53f0e3000779
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-05 13:44:18 +0600

    NavBar Updated

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,23 +2,25 @@ import { Link, NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   const linkClasses = ({ isActive }) =>
-    isActive ? 'nav-link active text-uppercase' : 'nav-link text-uppercase';
+    isActive
+      ? 'nav-link active text-uppercase py-0 fw-medium'
+      : 'nav-link text-uppercase py-0';
 
   return (
     <nav className='navbar d-flex align-items-center justify-content-between'>
-      <Link to='/' className='navbar-brand fw-bold text-uppercase'>
-        Todo App
+      <Link to='/' className='navbar-brand fw-bold text-uppercase me-0 py-0'>
+        <small>Todo App</small>
       </Link>
 
       <ul className='navbar-nav flex-row gap-4'>
         <li className='nav-item'>
           <NavLink to='/' className={linkClasses}>
-            Home
+            <small>Home</small>
           </NavLink>
         </li>
         <li className='nav-item'>
           <NavLink to='/about' className={linkClasses}>
-            About
+            <small>About</small>
           </NavLink>
         </li>
       </ul>
