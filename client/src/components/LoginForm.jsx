01e5f056e0b8261dbf60f41a19f4354b480d2394
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-06 10:07:34 +0600

    Login Form

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import { useState } from 'react';
 import InputField from './InputField';
 
 const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <form>
+    <form className='d-flex flex-column gap-3'>
       <InputField
         type='email'
         name='email'
         id='email'
         classes='form-control'
         placeholder='Enter Email Address'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <InputField
+        type='password'
+        name='password'
+        id='password'
+        classes='form-control'
+        placeholder='Enter Password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
       />
     </form>
   );
