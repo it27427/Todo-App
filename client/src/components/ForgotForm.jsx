a045d5e6373f3d5f00c950d09c0f60a7e4b108d5
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-06 11:28:21 +0600

    Forgot-Password Form Completed

--- client/src/components/ForgotForm.jsx
+++ client/src/components/ForgotForm.jsx
@@ -1,7 +1,30 @@
+import { useState } from 'react';
+import Button from './Button';
+import InputField from './InputField';
+
 const ForgotForm = () => {
+  const [email, setEmail] = useState('');
+
   return (
-    <form>
-      <h4>Forgot Form</h4>
+    <form className='d-flex flex-column gap-3 w-100 bg-white px-4 py-5 rounded'>
+      <InputField
+        type='email'
+        name='email'
+        id='email'
+        classes='form-control'
+        placeholder='Enter Email Address'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <div className='mt-3'>
+        <Button
+          type='submit'
+          classes='btn btn-primary text-uppercase fw-semibold w-100'
+        >
+          <small>Continue</small>
+        </Button>
+      </div>
     </form>
   );
 };
